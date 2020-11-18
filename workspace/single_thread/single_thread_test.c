98bb79a3d457036454b15033809d00e7821300e4
Luigi-Crisci <luigicrisci1997@gmail.com>
2020-11-18 19:08:25 +0100

    Removed __asm function in push

--- workspace/single_thread/single_thread_test.c
+++ workspace/single_thread/single_thread_test.c
@@ -10,8 +10,8 @@ struct coppia{
 
 int main(){
 	struct lfds711_stack_state stack;	
-	struct lfds711_stack_element element;
-	struct coppia *c;
+	struct lfds711_stack_element element,element2,element3;
+	struct coppia *c,*c1,*c2;
 
 	lfds711_stack_init_valid_on_current_logical_core(&stack,NULL);
 
@@ -21,13 +21,29 @@ int main(){
 	
 
 	LFDS711_STACK_SET_VALUE_IN_ELEMENT(element,c);
-	 int aaa = ((struct coppia*)LFDS711_STACK_GET_VALUE_FROM_ELEMENT(element))->x;
+	int aaa = ((struct coppia*)LFDS711_STACK_GET_VALUE_FROM_ELEMENT(element))->x;
 	lfds711_stack_push(&stack,&element);
 
-	// struct lfds711_stack_element *e;
-	// int res = lfds711_stack_pop(&stack,&e);
-	// struct coppia *result = (struct coppia*)LFDS711_STACK_GET_VALUE_FROM_ELEMENT(*e); 
-	// printf("Result %d : %d\n",result->x,result->y);
+	c1 = (struct coppia*)malloc(sizeof(struct coppia));
+	c1->y = 4;
+	c1->x = 4;
+	
+
+	LFDS711_STACK_SET_VALUE_IN_ELEMENT(element2,c1);
+	lfds711_stack_push(&stack,&element2);
+
+	c2 = (struct coppia*)malloc(sizeof(struct coppia));
+	c2->y = 6;
+	c2->x = 6;
+	
+
+	LFDS711_STACK_SET_VALUE_IN_ELEMENT(element3,c2);
+	lfds711_stack_push(&stack,&element3);
+
+	struct lfds711_stack_element *e;
+	int res = lfds711_stack_pop(&stack,&e);
+	struct coppia *result = (struct coppia*)LFDS711_STACK_GET_VALUE_FROM_ELEMENT(*e); 
+	printf("Result %d : %d\n",result->x,result->y);
 
 	// // lfds711_stack_cleanup(&stack,NULL);
 	// int s = result->x;
