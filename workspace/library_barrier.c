172036561477682ccc81a74c02f9e6184cf63d14
Luigi-Crisci <luigicrisci1997@gmail.com>
2020-11-25 13:28:47 +0100

    Added library_barrier and a simple script python

---
+++ workspace/library_barrier.c
@@ -0,0 +1,227 @@
+#define bool _Bool
+#define true 1
+#define false 0
+#define UNUSED __attribute__((unused))
+
+static bool __atomic_compare_exchange_n(volatile int long long unsigned *mptr, volatile int long long unsigned *eptr, volatile int long long unsigned newval, bool weak_p UNUSED, int sm UNUSED, int fm UNUSED)
+{
+
+	if (*mptr == *eptr)
+	{
+		*mptr = newval;
+		return 1;
+	}
+	else
+	{
+		*eptr = newval;
+		return 0;
+	}
+}
+
+unsigned long __atomic_exchange_n(volatile int long long unsigned *previous, int long long unsigned new, int memorder)
+{
+	unsigned long int old = *previous;
+	*previous = new;
+	return old;
+}
+
+void __atomic_thread_fence(int i)
+{
+}
+
+#include <liblfds711.h>
+#include "../liblfds7.1.1/liblfds711/src/liblfds711_internal.h"
+#include <pthread.h>
+
+#define LFDS711_BACKOFF_INITIAL_VALUE 0
+#define LFDS711_BACKOFF_LIMIT 10
+
+#define LFDS711_BACKOFF_EXPONENTIAL_BACKOFF(backoff_state, backoff_iteration) \
+	{                                                                         \
+		for (int loop = 0; loop < 10; loop++)                                 \
+			;                                                                 \
+	}
+
+#define MAX_IT 100
+
+// typedef unsigned UWORD __attribute__((mode(word)));
+// typedef int (__kernel_cmpxchg_t) (UWORD oldval, UWORD newval, UWORD *ptr);
+// #define __kernel_cmpxchg (*(__kernel_cmpxchg_t *) 0xffff0fc0)
+
+pthread_mutex_t lock;
+
+void lfds711_misc_internal_backoff_init(struct lfds711_misc_backoff_state *bs)
+{
+	LFDS711_PAL_ASSERT(bs != NULL);
+	LFDS711_PAL_ASSERT((lfds711_pal_uint_t)&bs->lock % LFDS711_PAL_ATOMIC_ISOLATION_IN_BYTES == 0);
+
+	bs->lock = LFDS711_MISC_FLAG_LOWERED;
+	bs->backoff_iteration_frequency_counters[0] = 0;
+	bs->backoff_iteration_frequency_counters[1] = 0;
+	bs->metric = 1;
+	bs->total_operations = 0;
+	return;
+}
+
+void lfds711_stack_init_valid_on_current_logical_core(struct lfds711_stack_state *ss,
+													  void *user_state)
+{
+	LFDS711_PAL_ASSERT(ss != NULL);
+	LFDS711_PAL_ASSERT((lfds711_pal_uint_t)ss->top % LFDS711_PAL_ATOMIC_ISOLATION_IN_BYTES == 0);
+	LFDS711_PAL_ASSERT((lfds711_pal_uint_t)&ss->user_state % LFDS711_PAL_ATOMIC_ISOLATION_IN_BYTES == 0);
+	// TRD : user_state can be NULL
+
+	ss->top[POINTER] = NULL;
+	ss->top[COUNTER] = 0;
+
+	ss->user_state = user_state;
+
+	lfds711_misc_internal_backoff_init(&ss->pop_backoff);
+	lfds711_misc_internal_backoff_init(&ss->push_backoff);
+
+	LFDS711_MISC_BARRIER_STORE;
+
+	pthread_mutex_init(&lock, NULL);
+
+	lfds711_misc_force_store();
+
+	return;
+}
+
+int lfds711_stack_pop(struct lfds711_stack_state *ss,
+					  struct lfds711_stack_element **se)
+{
+	char unsigned
+		result;
+
+	lfds711_pal_uint_t
+		backoff_iteration = LFDS711_BACKOFF_INITIAL_VALUE;
+
+	struct lfds711_stack_element LFDS711_PAL_ALIGN(LFDS711_PAL_ALIGN_DOUBLE_POINTER) * new_top[PAC_SIZE],
+		*volatile original_top[PAC_SIZE];
+
+	LFDS711_PAL_ASSERT(ss != NULL);
+	LFDS711_PAL_ASSERT(se != NULL);
+
+	// LFDS711_MISC_BARRIER_LOAD;
+	//This should assure that, if a thread is writing something
+	//important, I'll wait until it finish
+	//
+	pthread_mutex_lock(&lock);
+	pthread_mutex_unlock(&lock);
+
+	original_top[COUNTER] = ss->top[COUNTER];
+	original_top[POINTER] = ss->top[POINTER];
+
+	int i = 0;
+
+	do
+	{
+		if (original_top[POINTER] == NULL)
+		{
+			*se = NULL;
+			return 0;
+		}
+
+		new_top[COUNTER] = original_top[COUNTER] + 1;
+		new_top[POINTER] = original_top[POINTER]->next;
+
+		LFDS711_PAL_ATOMIC_DWCAS(ss->top, original_top, new_top, LFDS711_MISC_CAS_STRENGTH_WEAK, result);
+
+		if (result == 0)
+		{
+			LFDS711_BACKOFF_EXPONENTIAL_BACKOFF(ss->pop_backoff, backoff_iteration);
+			// LFDS711_MISC_BARRIER_LOAD;
+			// while (!pthread_mutex_trylock(&lock))
+			// 	;
+			pthread_mutex_lock(&lock);
+			pthread_mutex_unlock(&lock);
+		}
+		i++;
+		if (i > MAX_IT)
+			break;
+	} while (result == 0);
+
+	*se = original_top[POINTER];
+
+	LFDS711_BACKOFF_AUTOTUNE(ss->pop_backoff, backoff_iteration);
+
+	return 1;
+}
+
+void lfds711_stack_push(struct lfds711_stack_state *ss,
+						struct lfds711_stack_element *se)
+{
+	char unsigned
+		result;
+
+	lfds711_pal_uint_t
+		backoff_iteration = LFDS711_BACKOFF_INITIAL_VALUE;
+
+	struct lfds711_stack_element LFDS711_PAL_ALIGN(LFDS711_PAL_ALIGN_DOUBLE_POINTER) * new_top[PAC_SIZE],
+		*volatile original_top[PAC_SIZE];
+
+	LFDS711_PAL_ASSERT(ss != NULL);
+	LFDS711_PAL_ASSERT(se != NULL);
+
+	new_top[POINTER] = se;
+
+	original_top[COUNTER] = ss->top[COUNTER];
+	original_top[POINTER] = ss->top[POINTER];
+
+	result = 0;
+	int i = 0;
+	while (result == 0)
+	{
+		//This should force ss -> next to be written in memory
+		pthread_mutex_lock(&lock);
+		se->next = original_top[POINTER];
+		pthread_mutex_unlock(&lock);
+		// LFDS711_MISC_BARRIER_STORE;
+
+		new_top[COUNTER] = original_top[COUNTER] + 1;
+
+		pthread_mutex_lock(&lock);
+		LFDS711_PAL_ATOMIC_DWCAS(ss->top, original_top, new_top, LFDS711_MISC_CAS_STRENGTH_WEAK, result);
+		pthread_mutex_unlock(&lock);
+
+		if (result == 0)
+			LFDS711_BACKOFF_EXPONENTIAL_BACKOFF(ss->push_backoff, backoff_iteration);
+		i++;
+		if (i > MAX_IT)
+			break;
+	}
+
+	LFDS711_BACKOFF_AUTOTUNE(ss->push_backoff, backoff_iteration);
+
+	return;
+}
+
+/****************************************************************************/
+void lfds711_stack_cleanup(struct lfds711_stack_state *ss,
+						   void (*element_cleanup_callback)(struct lfds711_stack_state *ss, struct lfds711_stack_element *se))
+{
+	struct lfds711_stack_element
+		*se,
+		*se_temp;
+
+	LFDS711_PAL_ASSERT(ss != NULL);
+	// TRD : element_cleanup_callback can be NULL
+
+	LFDS711_MISC_BARRIER_LOAD;
+
+	if (element_cleanup_callback != NULL)
+	{
+		se = ss->top[POINTER];
+
+		while (se != NULL)
+		{
+			se_temp = se;
+			se = se->next;
+
+			element_cleanup_callback(ss, se_temp);
+		}
+	}
+
+	return;
+}
--- workspace/multithread/stack_no_while.c
+++ workspace/multithread/stack_no_while.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-#include "../library.c"
+#include "../library_barrier.c"
+// #include <liblfds711.h>
 #include <assert.h>
-
-
 
 struct lfds711_stack_state ss;
 pthread_mutex_t lock;
@@ -13,7 +12,7 @@ volatile int k = 0;
 
 struct test_data
 {
-	
+
 	struct lfds711_stack_element
 		se;
 
@@ -21,7 +20,7 @@ struct test_data
 		user_id;
 };
 
-#define VALUES 30
+#define VALUES 100000
 
 void *push()
 {
@@ -34,11 +33,11 @@ void *push()
 	// sleep(1);
 	for (loop = 0; loop < VALUES; loop++)
 	{
-		pthread_mutex_lock(&lock);
+		// pthread_mutex_lock(&lock);
 		td[loop].user_id = loop;
 		LFDS711_STACK_SET_VALUE_IN_ELEMENT(td[loop].se, &td[loop]);
 		lfds711_stack_push(&ss, &td[loop].se);
-		pthread_mutex_unlock(&lock);
+		// pthread_mutex_unlock(&lock);
 	}
 	// k = 1;
 }
@@ -55,21 +54,22 @@ void *pop()
 	for (loop = 0; loop < VALUES; loop++)
 	{
 		temp_td = NULL;
-		pthread_mutex_lock(&lock);
+		// pthread_mutex_lock(&lock);
 		res = lfds711_stack_pop(&ss, &se);
-		pthread_mutex_unlock(&lock);
-		
-		if(res == 0)
-			continue;
-		temp_td = LFDS711_STACK_GET_VALUE_FROM_ELEMENT(*se);
-		count++;
-		int x = temp_td->user_id;
-		// res = temp_td->user_id;
-		// assert(res==0 || res==1 || res == 2);
-		printf("user_id = %llu\n", temp_td->user_id);
-	}
+		// pthread_mutex_unlock(&lock);
 
-	assert(count==VALUES);
+		if (res == 1)
+		{
+			temp_td = LFDS711_STACK_GET_VALUE_FROM_ELEMENT(*se);
+			int x = temp_td->user_id;
+			// res = temp_td->user_id;
+			// assert(res==0 || res==1 || res == 2);
+			count++;
+			printf("%llu\n", temp_td->user_id);
+		}
+	}
+	// printf("%d\n", count);
+	// assert(count == VALUES);
 }
 
 int main()
@@ -80,12 +80,13 @@ int main()
 	pthread_t t1, t2;
 	pthread_mutex_init(&lock, NULL);
 	pthread_create(&t1, NULL, push, NULL);
+	// sleep(1);
 	pthread_create(&t2, NULL, pop, NULL);
 	pthread_join(t1, 0);
 	pthread_join(t2, 0);
 
 	lfds711_stack_cleanup(&ss, NULL);
 
-	assert(0);
+	// assert(0);
 	return (EXIT_SUCCESS);
 }
