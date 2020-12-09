ce9f2e9d6a300525b6407c65334217b5f4643950
Luigi-Crisci <luigicrisci1997@gmail.com>
2020-12-09 09:58:50 +0100

    Stack interface added

--- workspace/library_barrier.c
+++ workspace/library_barrier.c
@@ -8,13 +8,7 @@
 #define UNUSED __attribute__((unused))
 // Change function names, so that they are treated as Atomic by Lazy-Cseq
 
-pthread_mutex_t lock;
-static bool __atomic_compare_exchange_n(volatile int long long unsigned *mptr, volatile int long long unsigned *eptr, volatile int long long unsigned newval, bool weak_p UNUSED, int sm UNUSED, int fm UNUSED){
-	// pthread_mutex_lock(&lock);
-	int res = __VERIFIER_atomic_compare_and_exchange(mptr,eptr,newval,weak_p,sm,fm);
-	// pthread_mutex_unlock(&lock);
-	return res;
-}
+pthread_mutex_t library_lock;
 
 static bool __VERIFIER_atomic_compare_and_exchange(volatile int long long unsigned *mptr, volatile int long long unsigned *eptr, volatile int long long unsigned newval, bool weak_p UNUSED, int sm UNUSED, int fm UNUSED)
 {
@@ -30,10 +24,11 @@ static bool __VERIFIER_atomic_compare_and_exchange(volatile int long long unsign
 	}
 }
 
-unsigned long __atomic_exchange_n(volatile int long long unsigned *previous, int long long unsigned new, int memorder){
-	// pthread_mutex_lock(&lock);
-	int res = __VERIFIER_atomic_exchange(previous,new,memorder);
-	// pthread_mutex_unlock(&lock);
+static bool __atomic_compare_exchange_n(volatile int long long unsigned *mptr, volatile int long long unsigned *eptr, volatile int long long unsigned newval, bool weak_p UNUSED, int sm UNUSED, int fm UNUSED)
+{
+	// pthread_mutex_lock(&library_lock);
+	int res = __VERIFIER_atomic_compare_and_exchange(mptr, eptr, newval, weak_p, sm, fm);
+	// pthread_mutex_unlock(&library_lock);
 	return res;
 }
 
@@ -44,30 +39,37 @@ unsigned long __VERIFIER_atomic_exchange(volatile int long long unsigned *previo
 	return old;
 }
 
+unsigned long __atomic_exchange_n(volatile int long long unsigned *previous, int long long unsigned new, int memorder)
+{
+	// pthread_mutex_lock(&library_lock);
+	int res = __VERIFIER_atomic_exchange(previous, new, memorder);
+	// pthread_mutex_unlock(&library_lock);
+	return res;
+}
 
 // static bool __atomic_compare_exchange_n(volatile int long long unsigned *mptr, volatile int long long unsigned *eptr, volatile int long long unsigned newval, bool weak_p UNUSED, int sm UNUSED, int fm UNUSED)
 // {
-// 	// pthread_mutex_lock(&lock);
+// 	// pthread_mutex_lock(&library_lock);
 // 	if (*mptr == *eptr)
 // 	{
 // 		*mptr = newval;
-// 		// pthread_mutex_unlock(&lock);
+// 		// pthread_mutex_unlock(&library_lock);
 // 		return 1;
 // 	}
 // 	else
 // 	{
 // 		*eptr = newval;
-// 		// pthread_mutex_unlock(&lock);
+// 		// pthread_mutex_unlock(&library_lock);
 // 		return 0;
 // 	}
 // }
 
 // unsigned long __atomic_exchange_n(volatile int long long unsigned *previous, int long long unsigned new, int memorder)
 // {
-// 	// pthread_mutex_lock(&lock);
+// 	// pthread_mutex_lock(&library_lock);
 // 	unsigned long int old = *previous;
 // 	*previous = new;
-// 	// pthread_mutex_unlock(&lock);
+// 	// pthread_mutex_unlock(&library_lock);
 // 	return old;
 // }
 
@@ -75,8 +77,8 @@ void __atomic_thread_fence(int i)
 {
 }
 
-int __VERIFIER_atomic_swap_stack_top(struct lfds711_stack_element * volatile* top, struct lfds711_stack_element * volatile* oldtop, 
-				struct lfds711_stack_element **newtop)
+int __VERIFIER_atomic_swap_stack_top(struct lfds711_stack_element *volatile *top, struct lfds711_stack_element *volatile *oldtop,
+									 struct lfds711_stack_element **newtop)
 {
 	if (*oldtop == *top)
 	{
@@ -90,13 +92,14 @@ int __VERIFIER_atomic_swap_stack_top(struct lfds711_stack_element * volatile* to
 	}
 }
 
-
 #define LFDS711_BACKOFF_INITIAL_VALUE 0
 #define LFDS711_BACKOFF_LIMIT 10
 
-void exponential_backoff(){
+void exponential_backoff()
+{
 	int loop;
-	for (loop = 0; loop < 10; loop++);     
+	for (loop = 0; loop < 10; loop++)
+		;
 }
 
 // #define LFDS711_BACKOFF_EXPONENTIAL_BACKOFF(backoff_state, backoff_iteration) \
@@ -110,8 +113,6 @@ void exponential_backoff(){
 // typedef unsigned UWORD __attribute__((mode(word)));
 // typedef int (__kernel_cmpxchg_t) (UWORD oldval, UWORD newval, UWORD *ptr);
 // #define __kernel_cmpxchg (*(__kernel_cmpxchg_t *) 0xffff0fc0)
-
-
 
 void lfds711_misc_internal_backoff_init(struct lfds711_misc_backoff_state *bs)
 {
@@ -129,15 +130,14 @@ void lfds711_misc_internal_backoff_init(struct lfds711_misc_backoff_state *bs)
 void lfds711_stack_init_valid_on_current_logical_core(struct lfds711_stack_state *ss,
 													  void *user_state)
 {
-	pthread_mutex_init(&lock, NULL);
+	pthread_mutex_init(&library_lock, NULL);
 
-	
 	LFDS711_PAL_ASSERT(ss != NULL);
 	LFDS711_PAL_ASSERT((lfds711_pal_uint_t)ss->top % LFDS711_PAL_ATOMIC_ISOLATION_IN_BYTES == 0);
 	LFDS711_PAL_ASSERT((lfds711_pal_uint_t)&ss->user_state % LFDS711_PAL_ATOMIC_ISOLATION_IN_BYTES == 0);
 	// TRD : user_state can be NULL
 
-	pthread_mutex_lock(&lock);
+	pthread_mutex_lock(&library_lock);
 	ss->top[POINTER] = NULL;
 	ss->top[COUNTER] = 0;
 
@@ -149,7 +149,7 @@ void lfds711_stack_init_valid_on_current_logical_core(struct lfds711_stack_state
 
 	lfds711_misc_force_store();
 
-	pthread_mutex_unlock(&lock);
+	pthread_mutex_unlock(&library_lock);
 
 	return;
 }
@@ -173,8 +173,8 @@ int lfds711_stack_pop(struct lfds711_stack_state *ss,
 	//This should assure that, if a thread is writing something
 	//important, I'll wait until it finish
 	//
-	pthread_mutex_lock(&lock);
-	pthread_mutex_unlock(&lock);
+	pthread_mutex_lock(&library_lock);
+	pthread_mutex_unlock(&library_lock);
 
 	original_top[COUNTER] = ss->top[COUNTER];
 	original_top[POINTER] = ss->top[POINTER];
@@ -193,16 +193,16 @@ int lfds711_stack_pop(struct lfds711_stack_state *ss,
 		new_top[POINTER] = original_top[POINTER]->next;
 
 		// LFDS711_PAL_ATOMIC_DWCAS(ss->top, original_top, new_top, LFDS711_MISC_CAS_STRENGTH_WEAK, result);
-		pthread_mutex_lock(&lock);
+		pthread_mutex_lock(&library_lock);
 		result = __VERIFIER_atomic_swap_stack_top(&(ss->top[0]), &(original_top[0]), &(new_top[0]));
-		pthread_mutex_unlock(&lock);
+		pthread_mutex_unlock(&library_lock);
 
 		if (result == 0)
 		{
 			// LFDS711_BACKOFF_EXPONENTIAL_BACKOFF(ss->pop_backoff, backoff_iteration);
 			exponential_backoff();
-			pthread_mutex_lock(&lock);
-			pthread_mutex_unlock(&lock);
+			pthread_mutex_lock(&library_lock);
+			pthread_mutex_unlock(&library_lock);
 		}
 		i++;
 		if (i > MAX_IT)
@@ -219,6 +219,7 @@ int lfds711_stack_pop(struct lfds711_stack_state *ss,
 void lfds711_stack_push(struct lfds711_stack_state *ss,
 						struct lfds711_stack_element *se)
 {
+	// fflush(STDOUT_FILENO);
 	char unsigned
 		result;
 
@@ -241,17 +242,19 @@ void lfds711_stack_push(struct lfds711_stack_state *ss,
 	while (result == 0)
 	{
 		//This should force ss -> next to be written in memory
-		pthread_mutex_lock(&lock);
+		// printf("Pre push lock\n");
+		pthread_mutex_lock(&library_lock);
+		// printf("Post push lock");
 		se->next = original_top[POINTER];
-		pthread_mutex_unlock(&lock);
+		pthread_mutex_unlock(&library_lock);
 		// LFDS711_MISC_BARRIER_STORE;
 
 		new_top[COUNTER] = original_top[COUNTER] + 1;
 
 		// LFDS711_PAL_ATOMIC_DWCAS(ss->top, original_top, new_top, LFDS711_MISC_CAS_STRENGTH_WEAK, result);
-		pthread_mutex_lock(&lock);
+		pthread_mutex_lock(&library_lock);
 		result = __VERIFIER_atomic_swap_stack_top(&(ss->top[0]), &(original_top[0]), &(new_top[0]));
-		pthread_mutex_unlock(&lock);
+		pthread_mutex_unlock(&library_lock);
 
 		if (result == 0)
 			// LFDS711_BACKOFF_EXPONENTIAL_BACKOFF(ss->push_backoff, backoff_iteration);
