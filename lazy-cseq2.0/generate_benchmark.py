0b8df8c516e8ae09f3979c1cfd24e1ea49cfee54
Silvio Corso <silvio.corso.98@gmail.com>
2020-12-13 13:54:43 +0100

    Fix "generate_benchmark.py" now it generates thread's operations properly

--- lazy-cseq2.0/generate_benchmark.py
+++ lazy-cseq2.0/generate_benchmark.py
@@ -1,47 +1,70 @@
 import sys
 from itertools import product, permutations, combinations
 
-def create_permutation(num_t,num_op):
-    array_perm = []
-    array_ops = []
-    list_perm = []
 
-    #Getting number of ops into an array
-    for i in range(0,int(num_op)):
-        array_ops.append(i)
-    print(array_ops)
+
+# Function to convert   
+def listToString(s):  
+    
+    # initialize an empty string 
+    str1 = " " 
+    
+    # return string   
+    return (str1.join(s)) 
+
+
+def create_permutation(num_t,num_op):
+
+    array_ops = [0,1]
+    array_perm = []
+    list_perm = []
+    thread_comb = []
+    
 
     #Creating for each thread the possible combinations of his operations
     for i in range(0, int(num_t)):
-       array_perm.append(product(array_ops,repeat=2))
+       array_perm.append(product(array_ops,repeat=int(num_op)))
     for i in range(0, len(array_perm)):
         list_perm.append(list(array_perm[i]))
-    print(list_perm)
+    #print(list_perm)
+
+    #Thread's operations cartesian product 
+    for x in list_perm:
+        thread_comb = product(x,repeat=int(num_t))
+    #print(help)
 
 
+    return thread_comb
 
-    #Trying to iterate through the elements (Caso base)
-    for th1 in list_perm[0]:
-        for th2 in list_perm[1]:
-            print(th1+th2)
-
-
-
-    return list_perm
-
+#Function to write into "generalized.c" a possible configuration of threads
 def create_benchmarks(perm):
-    count_thread = 1
-    for x in perm:
-        for a in x:
-            print("void *thread"+str(count_thread)+"{")
-            count_thread += 1
-            for k in a:
-                if(k % 2 == 0):
-                    print("insert()")
-                else:
-                    print("delete()")
-            print("}")
+    id = 0
+    string_thread_comb = []
+    temp_string = []
+    index = 0
 
+    #Iterate through the cartesian product obtained previously and convert it into c code
+    for x in perm:
+        count_thread = 1
+        for thread_seq in x:
+            temp_string.append("void *thread"+str(count_thread)+"{\nLOCK;\n")
+            count_thread += 1
+            for k in thread_seq:
+                if(k == 0):
+                    temp_string.append("insert("+str(id)+");\n")
+                    id+=1
+                else:
+                    temp_string.append("delete();\n")
+            temp_string.append("UNLOCK;\n")
+            temp_string.append("}\n")
+        string_thread_comb.insert(index,listToString(temp_string))
+        temp_string = []
+        index+=1
+
+    for x in string_thread_comb:
+        print(x)
+
+    generalized = open("../workspace/multithread/generalized.c")
 
 if __name__ == "__main__":
     num_thread = sys.argv[1]
